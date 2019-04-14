bec8e21017c7699b52c6f37169e2cb3560224814
Ramiro Magno <ramiro.magno@gmail.com>
2019-04-14 18:40:49 +0100

    Added functions for slow/fast testing.
    Former-commit-id: 4d1013c5d8e978a25ce01a909a451ec122fc2875

---
+++ R/tests.R
@@ -0,0 +1,44 @@
+#' Setup the environment to skip slow tests
+#'
+#' This function sets an environment variable \code{TEST_FAST} to 'true' which
+#' is used to flag if a test should be skipped or not. If you place
+#' \code{\link[gwasrapidd]{skip_if_testing_is_fast}} in a test then it will
+#' check if \code{TEST_FAST} is 'true', if it is, then it will skip the next
+#' expectations within a \code{test_that()} block.
+#' Please note that this function is to be used in gwasrapidd development.
+#'
+#' @export
+set_testing_fast <- function() {
+  Sys.setenv(TEST_FAST = "true")
+}
+
+#' Setup the environment to still run slow tests
+#'
+#' This function sets an environment variable \code{TEST_FAST} to 'false' which is used
+#' to flag if a test should be skipped or not. If you place
+#' \code{\link[gwasrapidd]{skip_if_testing_is_fast}} in a test then it will
+#' check if \code{TEST_FAST} is 'true', if it is, then it will skip the next
+#' expectations with a \code{test_that()} block.
+#' Please note that this function is to be used in gwasrapidd development.
+#'
+#' @export
+set_testing_slow <- function() {
+  Sys.setenv(TEST_FAST = "false")
+}
+
+#' Skips a test if TEST_FAST is 'true'
+#'
+#' This function checks the value of \code{TEST_FAST}, if it is 'true' it skips
+#' the test otherwise it still runs the following expectations.
+#' Please note that this function is to be used in gwasrapidd development.
+#'
+#' @export
+skip_if_testing_is_fast <-function()
+{
+  # If slow we return TRUE, so that the following expectations are run.
+  if (!identical(Sys.getenv("TEST_FAST"), "true")) {
+    return(invisible(TRUE))
+  }
+  # If fast, then we skip the next expectations.
+  skip("¡Arriba, arriba! ¡Ándale, ándale!")
+}
---
+++ tests/testthat/test-tests.R
@@ -0,0 +1,43 @@
+context("test-tests")
+
+test_that('set_testing_fast', {
+  # save current value of TEST_FAST
+  test_fast <- Sys.getenv("TEST_FAST")
+  # Now erase it.
+  Sys.unsetenv("TEST_FAST")
+  expect_true(set_testing_fast())
+  expect_identical(Sys.getenv("TEST_FAST"), 'true')
+  # Restore the original value before the test
+  Sys.setenv(TEST_FAST = test_fast)
+})
+
+test_that('set_testing_slow', {
+  # save current value of TEST_FAST
+  test_fast <- Sys.getenv("TEST_FAST")
+  # Now erase it.
+  Sys.unsetenv("TEST_FAST")
+  expect_true(set_testing_slow())
+  expect_identical(Sys.getenv("TEST_FAST"), 'false')
+  # Restore the original value before the test
+  Sys.setenv(TEST_FAST = test_fast)
+})
+
+test_that('skip_if_testing_is_fast: TEST_FAST is set to true', {
+  # save current value of TEST_FAST
+  test_fast <- Sys.getenv("TEST_FAST")
+  # Now erase it.
+  set_testing_fast()
+  with_mock(`testthat::skip` = function(...) FALSE, expect_false(skip_if_testing_is_fast()))
+  # Restore the original value before the test
+  Sys.setenv(TEST_FAST = test_fast)
+})
+
+test_that('skip_if_testing_is_fast: TEST_FAST is set to false', {
+  # save current value of TEST_FAST
+  test_fast <- Sys.getenv("TEST_FAST")
+  # Now erase it.
+  set_testing_slow()
+  expect_true(skip_if_testing_is_fast())
+  # Restore the original value before the test
+  Sys.setenv(TEST_FAST = test_fast)
+})
