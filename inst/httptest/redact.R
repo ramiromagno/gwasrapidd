20503fe742d161fbca823309f69c4f8063ca24b0
Ramiro Magno <ramiro.magno@gmail.com>
2019-04-17 17:14:10 +0100

    Cleaned up httptest config files.
    Former-commit-id: 5a684c7e44e0a05e07afbecf144b8860cd79da25

--- inst/httptest/redact.R
+++ inst/httptest/redact.R
@@ -1,18 +1,3 @@
-# function (response) {
-#   require(magrittr, quietly=TRUE)
-#   response %>%
-#     httptest::gsub_response("https\\://www.ebi.ac.uk/gwas/rest/api/", "gc/")
-# }
-
-
-# httptest::set_redactor(
-#   list(
-#     function (response) { httptest::gsub_response(response, "https\\://www.ebi.ac.uk/gwas/rest/api/", "gc/") },
-#     function (response) { httptest::gsub_response(response, "https:\\://www.ebi.ac.uk/efo/", "efo/") },
-#     function (response) { httptest::gsub_response(response, "https:\\://www.ebi.ac.uk/ols/api/ontologies/efo", "ols/") }
-#   )
-# )
-
 httptest::set_redactor(function (response) {
   response %>%
     httptest::gsub_response("^https\\://www.ebi.ac.uk/gwas/rest/api/studies/", "gc/s/") %>%
--- inst/httptest/request.R
+++ inst/httptest/request.R
@@ -1,17 +1,3 @@
-# function (request) {
-#   require(magrittr, quietly=TRUE)
-#   request %>%
-#     httptest::gsub_request("https\\://www.ebi.ac.uk/gwas/rest/api/", "gc/")
-# }
-
-# httptest::set_requester(
-#   list(
-#     function (request) { httptest::gsub_request(request, "https\\://www.ebi.ac.uk/gwas/rest/api/", "gc/") },
-#     function (request) { httptest::gsub_request(request, "https:\\://www.ebi.ac.uk/efo/", "efo/") },
-#     function (request) { httptest::gsub_request(request, "https:\\://www.ebi.ac.uk/ols/api/ontologies/efo", "ols/") }
-#   )
-# )
-
 httptest::set_requester(function (response) {
   response %>%
     httptest::gsub_request("^https\\://www.ebi.ac.uk/gwas/rest/api/studies/", "mocks/gc/s/") %>%
