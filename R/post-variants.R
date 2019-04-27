36bfe402b801c93bdf235340f8ca3406c3b673d4
Ramiro Magno <ramiro.magno@gmail.com>
2019-04-27 23:03:52 +0100

    Removed unnecessary comments in post-variants.R.
    Former-commit-id: 9a98069803c6f7391c00d6a406d9fc1edc19c0db

--- R/post-variants.R
+++ R/post-variants.R
@@ -10,11 +10,6 @@ open_in_dbsnp <- function(variant_id) {
   if (!(rlang::is_character(variant_id) ))
     stop("variant_id must be a character vector.")
 
-  # if (any(!is_pubmed_id(pubmed_id2)))
-  #   stop("These are not valid PubMed IDs: ",
-  #        concatenate::cc_and(pubmed_id[!is_pubmed_id(pubmed_id2)], oxford = TRUE),
-  #        ".")
-
   urls <-
     glue::glue("https://www.ncbi.nlm.nih.gov/snp/{variant_id}")
 
@@ -34,11 +29,6 @@ open_in_dbsnp <- function(variant_id) {
 open_in_gtex <- function(variant_id) {
   if (!(rlang::is_character(variant_id) ))
     stop("variant_id must be a character vector.")
-
-  # if (any(!is_pubmed_id(pubmed_id2)))
-  #   stop("These are not valid PubMed IDs: ",
-  #        concatenate::cc_and(pubmed_id[!is_pubmed_id(pubmed_id2)], oxford = TRUE),
-  #        ".")
 
   urls <-
     glue::glue("https://gtexportal.org/home/snp/{variant_id}")
