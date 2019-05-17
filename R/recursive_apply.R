9d3b481c4d36e36baa296f00d6bd52b974674e6e
Ramiro Magno <ramiro.magno@gmail.com>
2019-05-17 18:03:09 +0100

    Reducing line width to less than 80 chars.
    Former-commit-id: 40d0e8872ebf76af8378711718752483e1f4682e

--- R/recursive_apply.R
+++ R/recursive_apply.R
@@ -4,14 +4,14 @@
 #' version \code{\link[base]{lapply}} but contrary to
 #' \code{\link[base]{rapply}}, \code{recursive_apply} does not ignore
 #' \code{\link[base]{NULL}} values. Each element of the list which is not itself
-#' a list is replaced by the result of applying \code{fn}. If down the line there
-#' are data.frames, then their class is preserved.
+#' a list is replaced by the result of applying \code{fn}. If down the line
+#' there are data.frames, then their class is preserved.
 #'
 #' @param x A list (of potentially many nested lists).
 #' @param fn A function of a single argument.
 #'
 #' @return A list whose non-list elements have been replaced by the result of
-#' applying \code{fn}.
+#'   applying \code{fn}.
 #'
 #' @keywords internal
 recursive_apply <- function(x, fn)
